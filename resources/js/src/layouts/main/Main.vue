a1bb4602eef8dd66a25153e4d54e7d6b6c897018
SoulOfKarma <gomez.soto.ricardo@gmail.com>
2022-05-12 14:25:06 -0400

    se quita funcion de jwt externa

--- resources/js/src/layouts/main/Main.vue
+++ resources/js/src/layouts/main/Main.vue
@@ -291,19 +291,9 @@ export default {
         },
         toggleHideScrollToTop(val) {
             this.hideScrollToTop = val;
-        },
-        cargarTokenExterno() {
-            let objeto = { run: "18499714-2", password: "1849" };
-            axios
-                .post(this.externalVal + "/api/auth/login", objeto)
-                .then(res => {
-                    sessionStorage.setItem("token_externo", res.data.token);
-                })
-                .catch(error => console.log(error));
         }
     },
     created() {
-        this.cargarTokenExterno();
         var aux = sessionStorage.getItem("run");
 
         if (aux == null) {
